1ce2e0771619910c0a457a43829fb20dce30b194
Jonathan Mällberg <Jonathan.mellberg@gmail.com>
2022-03-02 14:58:49 +0100

    Update variables.tf

--- app-dev/variables.tf
+++ app-dev/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   description = "The name of the resource group. Must be unique on your Azure subscription."
-  #default    = "playground"
+  default    = "playground"
 }
 
 variable "location" {
